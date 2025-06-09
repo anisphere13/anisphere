fc49793c47d2fce0664ef84ca6f55dbb362d0b26
anisphere13 <anisphere13@gmail.com>
2025-06-09 17:42:42 +0200

    Sauvegarde automatique 2025-06-09 17:42

---
+++ test/identite/unit/mock_services.dart
@@ -0,0 +1,6 @@
+import 'package:mockito/annotations.dart';
+import 'package:anisphere/modules/identite/services/identity_service.dart';
+import 'package:anisphere/modules/identite/services/photo_verification_service.dart';
+
+@GenerateMocks([IdentityService, PhotoVerificationService])
+void main() {}
