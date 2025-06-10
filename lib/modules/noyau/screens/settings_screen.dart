850c3d59b11329577a2defac15022f40fa8b7c61
anisphere13 <anisphere13@gmail.com>
2025-06-10 18:01:47 +0200

    refactor: remove unused import for modules_service

--- lib/modules/noyau/screens/settings_screen.dart
+++ lib/modules/noyau/screens/settings_screen.dart
@@ -4,7 +4,6 @@ library;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../services/local_storage_service.dart';
-import '../services/modules_service.dart';
 import '../services/backup_service.dart';
 import '../providers/user_provider.dart';
 import '../services/animal_service.dart';
