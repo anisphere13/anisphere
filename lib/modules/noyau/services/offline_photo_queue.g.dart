c96acc6ffb8e461616ea3eee90280339a0b089ba
anisphere13 <anisphereapp@gmail.com>
2025-06-13 18:23:34 +0200

    feat(noyau): add PhotoTask queue

--- lib/modules/noyau/services/offline_photo_queue.g.dart
+++ lib/modules/noyau/services/offline_photo_queue.g.dart
@@ -38,3 +38,40 @@ class QueuedPhotoAdapter extends TypeAdapter<QueuedPhoto> {
           runtimeType == other.runtimeType &&
           typeId == other.typeId;
 }
+
+class PhotoTaskAdapter extends TypeAdapter<PhotoTask> {
+  @override
+  final int typeId = 132;
+
+  @override
+  PhotoTask read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return PhotoTask(
+      photo: fields[0] as PhotoModel,
+      timestamp: fields[1] as DateTime,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, PhotoTask obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(0)
+      ..write(obj.photo)
+      ..writeByte(1)
+      ..write(obj.timestamp);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is PhotoTaskAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
