bf44ca2dbacaa222a0eaccea46a3f831628c7ba8
anisphere13 <anisphereapp@gmail.com>
2025-06-15 11:30:09 +0200

    fix: import main models in g tests

--- test/noyau/unit/animal_model.g_test.dart
+++ test/noyau/unit/animal_model.g_test.dart
@@ -1,7 +1,7 @@
 // Copilot Prompt : Test automatique généré pour animal_model.g.dart (unit)
 import 'package:flutter_test/flutter_test.dart';
 import '../../test_config.dart';
-import 'package:anisphere/modules/noyau/models/animal_model.g.dart';
+import 'package:anisphere/modules/noyau/models/animal_model.dart';
 
 void main() {
   setUpAll(() async {
