abe10454e185570af8e8167514d2185f78775d91
anisphere13 <anisphereapp@gmail.com>
2025-06-21 14:56:35 +0200

    Use retrieved status in modules summary

--- lib/modules/noyau/services/modules_summary_service.dart
+++ lib/modules/noyau/services/modules_summary_service.dart
@@ -41,7 +41,7 @@ class ModulesSummaryService {
     for (final module in ModulesService.modules) {
       final status = statuses[module.id] ?? "disponible";
 
-      if (ModulesService.isActive(module.id)) {
+      if (status == 'actif') {
         switch (module.name) {
           case "Santé":
             summaries.add(
